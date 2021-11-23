46321678b2c48728b036e0d9e43cac476b0ecd41
gyanendrav2 <gyanendrav2@gmail.com>
2021-11-23 18:56:06 +0530

    added the api endpoint

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 app.use(cors());
 
 app.get("/", async (req, res) => {
+  
+  return res.status(200).send('<div><h1>Server is running</h1><h6>API Endpoints</h6><p>/google-suggestion</p></div>');
+});
+
+app.get("/google-suggestion", async (req, res) => {
   const { q } = req.query;
   const result = await request(
     `https://suggestqueries.google.com/complete/search?client=firefox&q=${q}`
