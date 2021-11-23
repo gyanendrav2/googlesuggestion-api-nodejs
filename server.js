a22476a7dd5079645f1d1cbc2e9781c89e3222f4
gyanendrav2 <gyanendrav2@gmail.com>
2021-11-23 19:03:34 +0530

    added the env port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;;
 const request = require("request-promise");
 const cors = require("cors");
 
